8559bfe9ee929e434d170db78c5a352fb5fe4bc4
Wadoyoka <atsushi021028@gmail.com>
2024-05-17 13:46:43 +0900

    課題5

--- ex5/DinnerFullCource.java
+++ ex5/DinnerFullCource.java
@@ -2,21 +2,25 @@ package ex5;
 
 public class DinnerFullCource {
     private Dish[] list = new Dish[5];// [0]-[4]の計5個
-    private String [] Dish_Name_List = {"ライス","ステーキ","親子丼","味噌汁","あじの塩焼き"};
-    private int [] Dish_Value_List ={100,600,500,300,600};
+
+    public DinnerFullCource() {
+        String[] Dish_Name_List = { "ライス", "ステーキ", "親子丼", "味噌汁", "あじの塩焼き" };
+        int[] Dish_Value_List = { 100, 600, 500, 300, 600 };
+        for (int i = 0; i < Dish_Name_List.length; i++) {
+            this.list[i] = new Dish();
+            this.list[i].setName(Dish_Name_List[i]);
+            this.list[i].setValune(Dish_Value_List[i]);
+        }
+    }
 
     public static void main(String[] args) {
-
         DinnerFullCource fullcourse = new DinnerFullCource();
         fullcourse.eatAll();
     }
 
-    public void eatAll(){
-        for (int i = 0; i < this.list.length; i++) {
-            this.list[i] = new Dish();
-            this.list[i].setName(this.Dish_Name_List[i]);
-            this.list[i].setValune(this.Dish_Value_List[i]);
-            System.out.println(this.list[i].getName()+":"+this.list[i].getValune()+"円");
+    public void eatAll() {
+        for (Dish dish: this.list) {
+            System.out.println(dish.getName() + ":" + dish.getValune() + "円");
         }
     }
 }
